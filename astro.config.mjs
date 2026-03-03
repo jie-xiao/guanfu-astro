8e38c91b5487bf18c1446cb9660f80a7f8154402
xyy <xyy@me.com>
2026-03-03 11:46:24 +0800

    fix: add base path, new pages, fix config syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://guanfuge.github.io',
+  site: 'https://jie-xiao.github.io/guanfu-astro',
   base: '/guanfu-astro/',
   output: 'static',
 });
