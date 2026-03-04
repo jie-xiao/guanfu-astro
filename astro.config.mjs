8fbfdad0b3169a79db05f798b61e2d6c8fb36fbb
xyy <xyy@me.com>
2026-03-04 11:09:56 +0800

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jie-xiao.github.io/guanfu-astro',
-  base: '/guanfu-astro/',
+  base: '',
   output: 'static',
 });
