135cf6d956d50352a3beb4b61cdc74fbc4bb104f
xyy <xyy@me.com>
2026-03-04 11:11:14 +0800

    fix: base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jie-xiao.github.io/guanfu-astro',
-  base: '',
+  base: '/guanfu-astro',
   output: 'static',
 });
