3b2d8bb22c3c05d3c2157d83e3f559053014c88b
xyy <xyy@me.com>
2026-03-14 08:34:21 +0800

    Updates

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://jie-xiao.github.io/guanfu-astro',
-  base: '/guanfu-astro',
-  output: 'static',
-});
